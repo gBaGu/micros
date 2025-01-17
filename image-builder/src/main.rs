b38e4b90a260bda1a8a68b1206ea629fbc274688
vadim <vadim.pysarenko97@gmail.com>
2025-01-17 23:06:15 +0200

    take kernel binary path from dependency

--- image-builder/src/main.rs
+++ image-builder/src/main.rs
@@ -2,13 +2,10 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let args = env::args().skip(1).collect();
-    let (kernel, out_dir) = match <Vec<_> as TryInto<[String; 2]>>::try_into(args) {
-        Ok([kernel, out_dir]) => (PathBuf::from(kernel), PathBuf::from(out_dir)),
-        _ => {
-            println!("2 arguments expected: kernel and out_dir");
-            return;
-        }
+    let kernel = PathBuf::from(env!("CARGO_BIN_FILE_MICROS_KERNEL"));
+    let Some(out_dir) = env::args().skip(1).next().map(PathBuf::from) else {
+        println!("out_dir argument is missing");
+        return;
     };
     std::fs::create_dir_all(out_dir.clone()).unwrap();
 
