045844eedf26a54c7f95f744a5ca5fe37515a03e
vadim <vadim.pysarenko97@gmail.com>
2025-01-19 17:19:42 +0200

    create bootloader_api entrypoint; put disk images next to the binary

--- image-builder/src/main.rs
+++ image-builder/src/main.rs
@@ -3,12 +3,9 @@ use std::path::PathBuf;
 
 fn main() {
     let kernel = PathBuf::from(env!("CARGO_BIN_FILE_MICROS_KERNEL"));
-    let Some(out_dir) = env::args().skip(1).next().map(PathBuf::from) else {
-        println!("out_dir argument is missing");
-        return;
-    };
-    std::fs::create_dir_all(out_dir.clone()).unwrap();
+    let out_dir = env::current_exe().unwrap().parent().unwrap().to_path_buf();
 
+    println!("creating disk images for kernel: {}", kernel.display());
     let uefi_path = out_dir.join("uefi.img");
     bootloader::UefiBoot::new(&kernel)
         .create_disk_image(&uefi_path)
--- micros-kernel/src/main.rs
+++ micros-kernel/src/main.rs
@@ -3,12 +3,18 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+bootloader_api::entry_point!(main);
+
+fn main(bootinfo: &'static mut bootloader_api::BootInfo) -> ! {
+    if let Some(framebuffer) = bootinfo.framebuffer.as_mut() {
+        for byte in framebuffer.buffer_mut() {
+            *byte = 0x90;
+        }
+    }
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
